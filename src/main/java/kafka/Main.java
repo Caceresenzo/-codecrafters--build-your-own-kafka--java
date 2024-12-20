efce91bfe9be2a59cc0f90075da6c3a4542d26ee
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-20 21:32:20 +0100

    pass 1st stage

---
+++ src/main/java/kafka/Main.java
@@ -0,0 +1,34 @@
+package kafka;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+	public static final int PORT = 9092;
+
+	public static void main(String[] args) {
+		Socket clientSocket = null;
+
+		System.out.println("listen: %d".formatted(PORT));
+		try (
+			final var serverSocket = new ServerSocket(PORT)
+		) {
+			serverSocket.setReuseAddress(true);
+
+			clientSocket = serverSocket.accept();
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+		} finally {
+			try {
+				if (clientSocket != null) {
+					clientSocket.close();
+				}
+			} catch (IOException e) {
+				System.out.println("IOException: " + e.getMessage());
+			}
+		}
+	}
+
+}
