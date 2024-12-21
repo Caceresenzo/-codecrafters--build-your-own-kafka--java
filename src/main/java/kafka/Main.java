08a4ad33842589b544171d6bda3752b55a230604
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-21 18:12:53 +0100

    pass 2nd stage

--- src/main/java/kafka/Main.java
+++ src/main/java/kafka/Main.java
@@ -1,5 +1,7 @@
 package kafka;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,6 +20,20 @@ public class Main {
 			serverSocket.setReuseAddress(true);
 
 			clientSocket = serverSocket.accept();
+			System.out.println("connected: %s".formatted(clientSocket.getRemoteSocketAddress()));
+
+			clientSocket.getInputStream().read();
+
+			final var responseOutputStream = new ByteArrayOutputStream();
+			final var dataOutputStream = new DataOutputStream(responseOutputStream);
+
+			dataOutputStream.writeInt(0);
+			dataOutputStream.writeInt(7);
+
+			clientSocket.getOutputStream().write(responseOutputStream.toByteArray());
+			clientSocket.getOutputStream().flush();
+
+			clientSocket.close();
 		} catch (IOException e) {
 			System.out.println("IOException: " + e.getMessage());
 		} finally {
